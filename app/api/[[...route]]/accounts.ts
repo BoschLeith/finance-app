33019ac41edb39699d08928a4ac9799a943b8067
BoschLeith <bosch.leith@gmail.com>
2024-06-06 17:36:02 -0500

    Created Accounts POST API

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,9 +1,11 @@
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { zValidator } from "@hono/zod-validator";
+import { eq } from "drizzle-orm";
 import { Hono } from "hono";
+import { createId } from "@paralleldrive/cuid2"
 
 import { db } from "@/db/drizzle";
-import { accounts } from "@/db/schema";
-import { eq } from "drizzle-orm";
+import { accounts, insertAccountSchema } from "@/db/schema";
 
 const app = new Hono()
   .get("/",
@@ -27,6 +29,28 @@ const app = new Hono()
       })
         .from(accounts)
         .where(eq(accounts.userId, auth.userId));
+
+      return c.json({ data });
+    })
+  .post(
+    "/",
+    clerkMiddleware(),
+    zValidator("json", insertAccountSchema.pick({
+      name: true,
+    })),
+    async (c) => {
+      const auth = getAuth(c);
+      const values = c.req.valid("json");
+
+      if (!auth?.userId) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+
+      const [data] = await db.insert(accounts).values({
+        id: createId(),
+        userId: auth.userId,
+        ...values
+      }).returning();
 
       return c.json({ data });
     });
